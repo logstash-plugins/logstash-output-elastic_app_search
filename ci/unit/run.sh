48cdd70d1dd83dac61a8503d0c62f856afd30a5c
Joao Duarte <jsvduarte@gmail.com>
2019-06-07 13:27:00 +0100

    support snapshot docker images for docker testing

--- ci/unit/run.sh
+++ ci/unit/run.sh
@@ -3,5 +3,4 @@
 # This is intended to be run inside the docker container as the command of the docker-compose.
 set -ex
 
-#bundle exec rspec -fd --pattern spec/**/*_spec.rb,spec/**/*_specs.rb
-bundle exec rspec -fd
+bundle exec rspec -fd --pattern spec/**/*_spec.rb,spec/**/*_specs.rb
